468dc1610022b8cfe321b7b14568f28986e7fa97
chrispuuri <97059331+chrispuuri@users.noreply.github.com>
2022-10-27 06:13:45 -0700

    Add files via upload

---
+++ models/raw_vault/hubs/hub_product.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='incremental')    }}
+
+{%- set source_model = "v_stg_products"  -%}
+{%- set src_pk = "HUB_PRODUCT_HKEY"      -%}
+{%- set src_nk = "PRODUCT_NAME"          -%}
+{%- set src_ldts = "LOAD_DATETIME"       -%}
+{%- set src_source = "RECORD_SOURCE"     -%}
+
+{{ dbtvault.hub(src_pk=src_pk, src_nk=src_nk, src_ldts=src_ldts,
+                src_source=src_source, source_model=source_model) }}
