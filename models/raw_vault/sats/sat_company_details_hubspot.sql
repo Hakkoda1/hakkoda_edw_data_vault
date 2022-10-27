5ee8e21eaff8debb8c04e88622c63bf4f163fca5
chrispuuri <97059331+chrispuuri@users.noreply.github.com>
2022-10-27 06:15:03 -0700

    Add files via upload

---
+++ models/raw_vault/sats/sat_company_details_hubspot.sql
@@ -0,0 +1,33 @@
+{{ config(materialized='vault_insert_by_rank', rank_column='DBTVAULT_RANK', rank_source_models='v_company') }}
+
+{%- set yaml_metadata -%}
+source_model: "v_company"
+src_pk: "HUB_COMPANY_HKEY"
+src_hashdiff: 
+  source_column: "SAT_COMPANY_DETAILS_HASHDIFF"
+  alias: "HASH_DIFF"
+src_payload:
+    exclude_columns: true
+    columns:
+        - "COMPANY_NAME"
+        - "PROPERTY_NAME"
+        - "dbt_scd_id"
+        - "dbt_updated_at"
+        - "dbt_valid_from" 
+        - "dbt_valid_to"
+        - "SAT_COMPANY_DETAILS_HASHDIFF"
+        - "DBTVAULT_RANK"
+src_eff: "EFFECTIVE_FROM"
+src_ldts: "LOAD_DATETIME"
+src_source: "RECORD_SOURCE"
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ dbtvault.sat(src_pk=metadata_dict["src_pk"],
+                src_hashdiff=metadata_dict["src_hashdiff"],
+                src_payload=metadata_dict["src_payload"],
+                src_eff=metadata_dict["src_eff"],
+                src_ldts=metadata_dict["src_ldts"],
+                src_source=metadata_dict["src_source"],
+                source_model=metadata_dict["source_model"])   }}
