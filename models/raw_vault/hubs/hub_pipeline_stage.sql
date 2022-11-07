6e15baa598bc02ae77664689be93ab53e1ec4692
chris_puuri@hakkoda.io <chris_puuri@hakkoda.io>
2022-11-07 19:38:53 +0000

    Added hubspot raw_vault objects

---
+++ models/raw_vault/hubs/hub_pipeline_stage.sql
@@ -0,0 +1,14 @@
+{{ config(
+    materialized='incremental'
+    ,tags=["hubspot"]
+    )    
+}}
+
+{%- set source_model = ["primed_hubspot_deal"]  -%}
+{%- set src_pk = "HUB_PIPELINE_STAGE_HKEY"      -%}
+{%- set src_nk = "PIPELINE_STAGE_NAME"          -%}
+{%- set src_ldts = "LOAD_DATETIME"       -%}
+{%- set src_source = "RECORD_SOURCE"     -%}
+
+{{ dbtvault.hub(src_pk=src_pk, src_nk=src_nk, src_ldts=src_ldts,
+                src_source=src_source, source_model=source_model) }}
