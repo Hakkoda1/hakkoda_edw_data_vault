54a9100a1194b6e31853229a1bf64ef34268f8fc
Chris Puuri <chris_puuri@hakkoda.io>
2022-11-12 14:50:27 +0000

    Added fivetran vars for engagement

--- tests/singular/hubspot/assert_deal_size_greater_than_zero_for_qualified_deals.sql
+++ tests/singular/hubspot/assert_deal_size_greater_than_zero_for_qualified_deals.sql
@@ -1,10 +1,10 @@
--- Deals have a non-zero amount if they are beyond the identified pipeline stage, so the total amount should always be >= 0.
+-- Deals have a non-null amount if they are beyond the identified pipeline stage, so the total amount should always be >= 0.
 -- Therefore return records where this isn't true to make the test fail.
 select
     deal_name,
     deal_owner,
     sum(amount) as total_amount
 from {{ ref('deals_analysis') }}
-where pipeline_stage <> 'Identified'
+where pipeline_stage NOT IN ('Identified', 'Closed Lost')
 group by 1,2
-having not(total_amount >= 0)
+having not(total_amount is not null)
