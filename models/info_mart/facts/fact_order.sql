4a4948e0029a97ff86d0e008209d4ada60bde1a0
Chris Puuri <chris_puuri@hakkoda.io>
2022-11-17 16:25:55 +0000

    Added Computed Sat for Payments and pushed to Fact Order

--- models/info_mart/facts/fact_order.sql
+++ models/info_mart/facts/fact_order.sql
@@ -2,6 +2,8 @@
     tags=["order","jaffle_shop"]
 ) }}
 
+{%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
 WITH LATEST_SAT_RECORD AS (
     SELECT *
     FROM {{ref('lsat_order_details_jaffle_shop')}} s
@@ -15,7 +17,10 @@ SELECT l.LNK_ORDER_CUSTOMER_HKEY FACT_ORDER_KEY
 ,ORDER_DATE
 ,STATUS ORDER_STATUS
 ,AMOUNT
+{% for payment_method in payment_methods -%},{{ payment_method }}_amount 
+{% endfor -%}
 FROM {{ref('lnk_order_customer')}} l
 INNER JOIN {{ref('hub_order')}} h ON l.HUB_ORDER_HKEY = h.HUB_ORDER_HKEY
 INNER JOIN LATEST_SAT_RECORD s ON l.LNK_ORDER_CUSTOMER_HKEY = s.LNK_ORDER_CUSTOMER_HKEY
 INNER JOIN {{ref('sat_order_amounts')}} oa ON l.HUB_ORDER_HKEY = oa.HUB_ORDER_HKEY
+INNER JOIN {{ref('sat_order_payments')}} op ON l.HUB_ORDER_HKEY = op.HUB_ORDER_HKEY
