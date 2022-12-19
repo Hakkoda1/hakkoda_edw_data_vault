3bd08086d58b2d6d9b76a8c4f43447f8328610ba
Chris Puuri <chris_puuri@hakkoda.io>
2022-12-19 16:43:48 +0000

    Updated dbt packages and finished debugging compare all columns sample

--- models/unit_test/jaffle_shop/tst_s2t_column_compare_values_jaffle_shop_customers.sql
+++ models/unit_test/jaffle_shop/tst_s2t_column_compare_values_jaffle_shop_customers.sql
@@ -8,20 +8,12 @@
 
 {% set target_model=ref('src_jaffle_shop_customers') %}
 
-with base_test_cte as (
-  {{ 
+{{ 
     audit_helper.compare_all_columns(
       a_relation=source_model,
       b_relation=target_model, 
       exclude_columns=['updated_at'], 
-      primary_key='id'
+      primary_key="ID"
     ) 
-  }}
-  left join {{ ref('stage_jaffle_shop_customers') }} using(id)
-  where conflicting_values
-)
-select
-  status, -- assume there's a "status" column in stg_customers
-  count(distinct case when conflicting_values then id end) as conflicting_values
-from base_test_cte
-group by 1
+}}
+where not perfect_match
