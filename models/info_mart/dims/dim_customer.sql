d5de160588d5e7fe9650839517ce5d3f8ae6411b
Chris Puuri <chris_puuri@hakkoda.io>
2023-01-12 16:46:32 +0000

    Fixed Dim Customer

--- models/info_mart/dims/dim_customer.sql
+++ models/info_mart/dims/dim_customer.sql
@@ -20,8 +20,8 @@ SELECT h.HUB_CUSTOMER_HKEY CUSTOMER_KEY
 ,LIFETIME_VALUE
 FROM {{ref('pit_customer')}} p
 INNER JOIN {{ref('hub_customer')}} h ON p.HUB_CUSTOMER_HKEY = h.HUB_CUSTOMER_HKEY
-INNER JOIN {{ref('hsat_customer_name_jaffle_shop')}} s1 ON p."hsat_customer_name_jaffle_shop_PK" = s1.HUB_CUSTOMER_HKEY AND s1.LOAD_DATETIME = p."hsat_customer_name_jaffle_shop_LDTS"
-INNER JOIN {{ref('hsat_customer_gaggle_user_details_crm')}} s2 ON p."hsat_customer_gaggle_user_details_crm_PK" = s2.HUB_CUSTOMER_HKEY AND s2.LOAD_DATETIME = p."hsat_customer_gaggle_user_details_crm_LDTS"
+INNER JOIN {{ref('hsat_customer_name_jaffle_shop')}} s1 ON p.hsat_customer_name_jaffle_shop_HKEY = s1.HUB_CUSTOMER_HKEY AND s1.LOAD_DATETIME = p.hsat_customer_name_jaffle_shop_LOAD_DATETIME
+INNER JOIN {{ref('hsat_customer_gaggle_user_details_crm')}} s2 ON p.hsat_customer_gaggle_user_details_crm_HKEY = s2.HUB_CUSTOMER_HKEY AND s2.LOAD_DATETIME = p.hsat_customer_gaggle_user_details_crm_LOAD_DATETIME
 LEFT JOIN {{ref('sat_customer_order_history')}} co ON h.HUB_CUSTOMER_HKEY = co.HUB_CUSTOMER_HKEY
 LEFT JOIN {{ref('sat_customer_gaggle_user_email_domains')}} ed ON h.HUB_CUSTOMER_HKEY = co.HUB_CUSTOMER_HKEY
 {{qualify_latest_satellite_records('p.HUB_CUSTOMER_HKEY', 'p.AS_OF_DATE')}} 
